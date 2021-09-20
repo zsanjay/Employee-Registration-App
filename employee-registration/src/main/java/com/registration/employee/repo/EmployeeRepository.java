79679de357dc30552b88b3b9e084f43c5b855c67
Sanjay Mehta <41693083+zsanjay@users.noreply.github.com>
2021-09-20 19:32:06 +0400

    CrudRepository changed to JpaRepository

--- employee-registration/src/main/java/com/registration/employee/repo/EmployeeRepository.java
+++ employee-registration/src/main/java/com/registration/employee/repo/EmployeeRepository.java
@@ -4,12 +4,12 @@ package com.registration.employee.repo;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
 
 import com.registration.employee.model.Employee;
 
-public interface EmployeeRepository extends CrudRepository<Employee , Long>{
+public interface EmployeeRepository extends JpaRepository<Employee , Long>{
 	
 	Employee findByEmpNo(Integer empNo);
 	
